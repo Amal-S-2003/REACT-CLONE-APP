3f28f43e42771580941620c58c9f7f75281721fb
Amal-S-2003 <amalshibu379@gmail.com>
2024-12-02 12:19:31 +0530

    FAQ

--- src/Components/FaqCard.jsx
+++ src/Components/FaqCard.jsx
@@ -1,36 +1,31 @@
 import React, { useState } from "react";
 
-function FaqCard() {
+function FaqCard({ item }) {
   const [view, setView] = useState(true);
   return (
     <>
       <div
-        className="col-12 px-4 py-2  d-flex align-items-center justify-content-between text-light fs-4 border-2 border-light rounded-3"
+        className="col-12 px-4 py-2 mt-3 d-flex align-items-center justify-content-between text-light fs-4 border-2 border-light rounded-3"
         style={{ backgroundColor: "gray" }}
         onClick={() => setView(!view)}
-
       >
-        <p className="mt-2">What is NetFlix</p>
+        <p className="mt-2">{item.qstn}</p>
         {view ? (
-          <button
-            className="bg-transparent border-0"
-          >
+          <button className="bg-transparent border-0">
             <i class="fa-solid fa-plus "></i>
           </button>
         ) : (
-          <button
-            className="bg-transparent border-0"
-          >
+          <button className="bg-transparent border-0">
             <i class="fa-solid fa-x"></i>
           </button>
         )}
       </div>
       {!view ? (
-        <div className="col-12 px-4 py-2 rounded-3 mt-3 f-4 text-light fw-bold"
-        style={{ backgroundColor: "gray" }}
->
-    <p className="fs-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi error quidem ex perferendis distinctio officiis enim ullam et praesentium odio pariatur voluptatibus accusamus atque debitis sequi cupiditate explicabo, quo nesciunt.</p>
-          
+        <div
+          className="col-12 px-4 py-2 rounded-3 mt-3 f-4 text-light fw-bold"
+          style={{ backgroundColor: "gray" }}
+        >
+          <p className="fs-5">{item.content}</p>
         </div>
       ) : (
         ""
