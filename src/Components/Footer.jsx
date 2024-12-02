edb848a6eea0f38ad1c48309ab1f93a7593704b5
Amal-S-2003 <amalshibu379@gmail.com>
2024-12-02 09:16:30 +0530

    Header ,Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <Container className="text-light py-4">
+        <p>
+          Questions?{" "}
+          <a href="#" className="text-light">
+            {" "}
+            Call 000-800-919-1694
+          </a>
+        </p>
+        <Row>
+          <div className="col-sm-12 col-lg-3 d-flex flex-column gap-3">
+            <a href="#" className="text-light">FAQ</a>
+            <a href="#" className="text-light">Investor Relations </a>
+            <a href="#" className="text-light"> Privacy </a>
+            <a href="#" className="text-light"> Speed Test</a>
+          </div>
+          <div className="col-sm-12 col-lg-3 d-flex flex-column gap-3">
+            <a href="#" className="text-light">Help Centre </a>
+            <a href="#" className="text-light">Jobs </a>
+            <a href="#" className="text-light">Cookie Preferences </a>
+            <a href="#" className="text-light"> Legal Notices</a>
+          </div>
+          <div className="col-sm-12 col-lg-3 d-flex flex-column gap-3">
+            <a href="#" className="text-light">Account</a>
+            <a href="#" className="text-light">Ways to Watch</a>
+            <a href="#" className="text-light">Corporate Information </a>
+            <a href="#" className="text-light">Only on Netflix</a>
+          </div>
+          <div className="col-sm-12 col-lg-3 d-flex flex-column gap-3">
+            <a href="#" className="text-light">Media Centre</a>
+            <a href="#" className="text-light">Terms of Use</a>
+            <a href="#" className="text-light">Contact Us</a>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/logo.png";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <nav className="d-flex flex-column align-items-center">
+        <div className="bg"></div>
+        <div className="header w-100 d-flex justify-content-between bg-transparent align-items-center px-3">
+          <img src={logo} alt="Logo" width={"180px"} />
+          <div className="btns d-flex align-items-center">
+            <select
+              name="lang"
+              id=""
+              className="langs fw-bold py-1  px-3 text-light bg-transparent"
+            >
+              <option value="eng" className="text-dark">
+                English
+              </option>
+              <option value="hin" className="text-dark">
+                Hindi
+              </option>
+            </select>
+            <button className="btn btn-light rounded-5 py-1 ms-3 px-3 fw-bold">
+              Sign in
+            </button>
+          </div>
+        </div>
+        <div className="banner m-3 ">
+          <h1 className="text-center text-light ">
+            Unlimited movies,TV <br />
+            shows and more
+          </h1>
+          <h5 className="text-center text-light">
+            Starts at $149.Cancel at any time.
+          </h5>
+        </div>
+      </nav>
+      <div className="box text-center py-5">
+        <p className="text-light">Ready to Watch?Enter your email to create or restart your membership.</p>
+        <div className="input">
+          <input className="py-2 fs-4  me-2 rounded-5 bg-transparent border-light border-1 px-5" type="text" placeholder="Email address" name="" id="" />
+          <button className="px-5 fs-4 py-2 fw-bold text-light rounded-5 border-0 " style={{backgroundColor:"red"}}>Join Now </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
